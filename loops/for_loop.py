f68f3233b2cbaee99728a51d435dd8f2f414f98c
Waqar Ahmad <wahmad@epteck.com>
2023-08-24 14:21:40 +0500

    learned loops

---
+++ loops/for_loop.py
@@ -0,0 +1,17 @@
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
+
+print('--------')
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+print('-----------')
+
+names = ['waqar', 'shafique', 'mushtaq', 'ahmad']
+for name in names:
+    print(name)

9e64865be8e5a7c7de8c8e9fbaedb91b554691b2
Waqar Ahmad <wahmad@epteck.com>
2023-08-24 13:50:20 +0500

    learned datatypes

---
+++ datatypes/datatypes.py
@@ -0,0 +1,10 @@
+rating = 4.5
+print(rating, type(rating))
+name = "Waqar"
+print(name, type(name))
+is_passed = True
+print(is_passed, type(is_passed))
+my_complex_number = 4 + 5j
+print(my_complex_number, type(my_complex_number))
+fruit_list = ['Banana', 'Apple', 'Kiwi']
+print(fruit_list, type(fruit_list))
